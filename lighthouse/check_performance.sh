b28638458cf159a01d1002377f4d68f438df63a0
BenedictNKosi01 <benedict.nkosi@merlinentertainments.biz>
2024-06-26 22:03:01 +0200

    Update check_performance.sh

--- lighthouse/check_performance.sh
+++ lighthouse/check_performance.sh
@@ -13,7 +13,7 @@ performance_score=$(jq -r '.categories.performance.score' report.json)
 performance_percentage=$(awk "BEGIN { printf \"%.2f\", $performance_score * 100 }")
 
 # Check if the performance score is above 90
-if (( $(awk "BEGIN { print ($performance_percentage > 90) }") )); then
+if (( $(awk "BEGIN { print ($performance_percentage > 99) }") )); then
   echo "Performance score is above 90: $performance_percentage"
   exit 0
 else
