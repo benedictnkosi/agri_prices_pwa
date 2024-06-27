6c5d13a7754aa2d299a8d3c772706816041fdc8d
BenedictNKosi01 <benedict.nkosi@merlinentertainments.biz>
2024-06-27 12:12:09 +0200

    lh

--- lighthouse/check_performance.sh
+++ lighthouse/check_performance.sh
@@ -13,12 +13,14 @@ best_practices_score=$(jq -r '.categories["best-practices"].score' report.json)
 
 # Convert the score to a percentage using awk
 performance_percentage=$(awk "BEGIN { printf \"%.2f\", $performance_score * 100 }")
+accessibility_percentage=$(awk "BEGIN { printf \"%.2f\", $performance_score * 100 }")
+best_practice_percentage=$(awk "BEGIN { printf \"%.2f\", $performance_score * 100 }")
 
 # Check if the performance score is above 90
-if (( $(awk "BEGIN { print ($performance_percentage > 90 && $accessibility_score > 90 && $best_practices_score > 90) }") )); then
-  echo "Lighthouse score failed, See html report"
+if (( $(awk "BEGIN { print ($performance_percentage > 90 && $accessibility_percentage > 90 && $best_practice_percentage > 90) }") )); then
+  echo "Lighthouse score is great"
   exit 0
 else
-  echo "Performance score is good, See html report"
+  echo "Lighthouse score failed"
   exit 1
 fi
