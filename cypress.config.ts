dea15b9b0cd26c37834d80898338e37d9332f81d
BenedictNKosi01 <benedict.nkosi@merlinentertainments.biz>
2024-07-01 10:11:06 +0200

    cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "43jo7w",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
