55e3d292c365c42d6ba1d291f6f54b75c131371a
BenedictNKosi01 <benedict.nkosi@merlinentertainments.biz>
2024-08-06 11:16:13 +0200

    chore: Add Cypress and Lighthouse tests to CI workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,20 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      const environment = config.env.ENV || 'dev'; // Default to 'dev' if ENV is not set
-
-      // Define base URLs for different environments
-      const baseUrls: { [key: string]: string } = {
-        dev: 'http://localhost:5173',
-      };
-
-      // Set the baseUrl based on the environment
-      config.baseUrl = baseUrls[environment];
-
-      return config;
-    },
+    baseUrl: process.env.BASEURL || 'http://localhost:5173',
     pageLoadTimeout: 10000,
   },
 });
