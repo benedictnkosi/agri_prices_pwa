14df5fe5d2e2b2c082a25f6ebfc8d209c634a649
BenedictNKosi01 <benedict.nkosi@merlinentertainments.biz>
2024-08-06 21:00:43 +0200

    chore: Update defaultCommandTimeout in Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,8 +6,9 @@ export default defineConfig({
   e2e: {
     baseUrl: process.env.BASEURL || 'http://localhost:5173',
     pageLoadTimeout: 20000,
-    requestTimeout: 10000,
-    responseTimeout: 10000,
+    requestTimeout: 20000,
+    responseTimeout: 20000,
+    defaultCommandTimeout: 20000,
     setupNodeEvents(on, config) {
       grep(config);
       return config;
