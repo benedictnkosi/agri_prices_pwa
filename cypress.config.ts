b63d27a584ef7ba94c76a3d82a2ad3519decad13
BenedictNKosi01 <benedict.nkosi@merlinentertainments.biz>
2024-08-21 20:22:10 +0200

    chore: Update pageLoadTimeout to 20000 for faster page loading

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ import * as process from 'process';
 export default defineConfig({
   e2e: {
     baseUrl: process.env.BASEURL || 'http://localhost:5173',
-    pageLoadTimeout: 60000,
+    pageLoadTimeout: 20000,
     requestTimeout: 20000,
     responseTimeout: 20000,
     defaultCommandTimeout: 20000,
@@ -17,4 +17,12 @@ export default defineConfig({
   env: {
     API_URL: process.env.API_URL, // Read the API_URL from the environment
   },
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0
+  }
 });
