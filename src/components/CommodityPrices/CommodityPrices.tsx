38f86b3a46c5890436c977c8ab13b2b1e1901c5e
BenedictNKosi01 <benedict.nkosi@merlinentertainments.biz>
2024-08-21 22:18:28 +0200

    chore: Update pageLoadTimeout for faster page loading

--- src/components/CommodityPrices/CommodityPrices.tsx
+++ src/components/CommodityPrices/CommodityPrices.tsx
@@ -8,7 +8,7 @@ import { TopProvinces } from "../TopProvinces/TopProvinces";
 import { PeriodFilter } from "../PeriodFilter/PeriodFilter";
 import { LineGraph } from "../LineGraph/LineGraph"; 
 import { PriceModel } from "../../models/PriceModel";
-import { WeightFilter } from "../WeightFilter/WeightFilter";
+import { DynamicFilter } from "../DynamicFilter/DynamicFilter";
 
 
 export const CommodityPrices = () => {
@@ -17,22 +17,41 @@ export const CommodityPrices = () => {
   const [error, setError] = useState<string | null>(null);
   const [period, setPeriod] = useState<string>("6");
   const [weight, setWeight] = useState<string>("");
-
+  const [grade, setGrade] = useState<string>("");
   const [prices, setPrices] = useState<PriceModel[]>([]);
-
+  const [filter, setFilter] = useState<FilterModel>({});
   const apiUrl = import.meta.env.VITE_API_URL;
   const pricesUrl = `${apiUrl}/public/prices`;
   
-  const filter: FilterModel = {
-    commodity: commodity || "",
-    grade: "",
-    weight: "",
-    period: "3"
-  }; 
+  
+  const handleGradeClick = (value: string) => {
+    if (value === grade) {
+      setGrade("");
+    }else{
+      setGrade(value);
+    }
+   
+  }
+
+  const handleWeightClick = (value: string) => {
+    if (value === weight) {
+      setWeight("");
+    }else{
+      setWeight(value);
+    }
+    
+  }
 
   useEffect(() => {
-    console.log(period);
-  }, [period]);
+    const filter: FilterModel = {
+      commodity: commodity || "",
+      grade: grade,
+      weight: weight,
+      period: period
+    }; 
+
+    setFilter(filter);
+  }, [commodity, grade, period, weight]);
 
   useEffect(() => {
     setLoading(true);
@@ -41,7 +60,7 @@ export const CommodityPrices = () => {
     .get(pricesUrl, {
       params: {
         crop: commodity,
-        grade: "",
+        grade: grade,
         weight: weight,
         period: period,
       },
@@ -50,9 +69,7 @@ export const CommodityPrices = () => {
       console.log(response.data);
       setPrices(response.data);
     });
-  }, [commodity, period, pricesUrl, weight]);
-
-
+  }, [commodity, period, pricesUrl, weight, grade]);
 
   if (loading) {
     return <div className="text-center">
@@ -76,7 +93,8 @@ export const CommodityPrices = () => {
       </div>
       <TopProvinces filter={filter} />
       <PeriodFilter setPeriod={setPeriod} period={period}/>
-      <WeightFilter filter={filter} setWeight={setWeight} weight={weight}/>
+      <DynamicFilter filter={filter} setValue={handleWeightClick} value={weight} field="weight" />
+      <DynamicFilter filter={filter} setValue={handleGradeClick} value={grade} field="grade" />
       <LineGraph prices={prices}/>
     </div>
     </>
