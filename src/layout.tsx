fae3e386dc5455613cd936a36013f70f20cf6d22
BenedictNKosi01 <benedict.nkosi@merlinentertainments.biz>
2024-08-22 16:09:25 +0200

    test

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import styles from "./layout.module.scss";
+import { Analytics } from "@vercel/analytics/react"
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,6 +9,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className={styles["layout"]}>
+      <Analytics />
       {/* <header></header> */}
       <main>{children}</main>
       <footer></footer>
